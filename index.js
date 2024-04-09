8d24de95a504d9fc1a323f79f3318fad6bbb3c86
Bernardo Araujo Nascimento <112490065+Bernardons04@users.noreply.github.com>
2024-04-09 19:19:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 let button = document.getElementById('button')
 let form = document.getElementById('form')
+var input = document.querySelectorAll('input')
+
+function calcular() {
+    let tokenPorSegundo = document.getElementById('idToken')
+    let custoHoraVM = document.getElementById('idCusto')
+    if (idToken.value != "" && idCusto.value != "") {
+        let milhaoToken = 1000000
+        let totalSegundo = milhaoToken/tokenPorSegundo.value
+        let totalTokenHora = totalSegundo/3600
+        let custoMilhaoTokenNossaVM = (custoHoraVM.value)*totalTokenHora
+        let custoResultado = document.getElementById('custoResultado')
+        if (custoMilhaoTokenNossaVM < 10) {
+            custoResultado.innerHTML = "Custo por 1M de tokens: $0" + custoMilhaoTokenNossaVM.toFixed(2)
+        } else {
+            custoResultado.innerHTML = "Custo por 1M de tokens: $" + custoMilhaoTokenNossaVM.toFixed(2)
+        }
+        
+        console.log('custo do milhao de token na nossa VM: ' + custoMilhaoTokenNossaVM.toFixed(2))
+    }
+}
 
 button.addEventListener('click', e => {
     calcular()
 })
-    
-function calcular() {
-    let tokenPorSegundo = document.getElementById('idToken')
-    let custoHoraVM = document.getElementById('idCusto')
-    let milhaoToken = 1000000
-    let totalSegundo = milhaoToken/tokenPorSegundo.value
-    let totalTokenHora = totalSegundo/3600
-    let custoMilhaoTokenNossaVM = (custoHoraVM.value)*totalTokenHora
-    let custoResultado = document.getElementById('custoResultado')
-    if (custoMilhaoTokenNossaVM < 10) {
-        custoResultado.innerHTML = "Custo por 1M de tokens: $0" + custoMilhaoTokenNossaVM.toFixed(2)
-    } else {
-        custoResultado.innerHTML = "Custo por 1M de tokens: $" + custoMilhaoTokenNossaVM.toFixed(2)
-    }
-
-    console.log('custo do milhao de token na nossa VM: ' + custoMilhaoTokenNossaVM.toFixed(2))
-}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
